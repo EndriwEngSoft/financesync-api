a7a7ec1455ba0a1970091ff2395598a073d305ff
Endriw Bento <endriwbento@gmail.com>
2026-03-17 21:26:34 -0300

    chore: project setup with Spring Boot, JPA, Security and PostgreSQL config

---
+++ src/main/java/com/endriw/financesync/Application.java
@@ -0,0 +1,13 @@
+package com.endriw.financesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
